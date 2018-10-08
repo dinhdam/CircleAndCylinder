f0dc4537ddb8f669021d2b1323b81a3944ed8efc
= <=>
2018-10-08 20:28:10 +0700

    hình tròn và hình trụ

---
+++ Module1/src/ClassCircle/CylinderTest.java
@@ -0,0 +1,10 @@
+package ClassCircle;
+
+public class CylinderTest {
+    public static void main(String[] args) {
+        Cylinder cylinder = new Cylinder();
+        System.out.println(cylinder);
+        cylinder = new Cylinder(25);
+        System.out.println(cylinder);
+    }
+}
